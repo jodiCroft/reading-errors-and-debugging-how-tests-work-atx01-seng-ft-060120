96f374fbced86a82fb7688de345d0408ea7f5c53
Jodi Croft <jodipruett@gmail.com>
2020-05-16 15:07:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 first_number = 4.55
-second_number = 0
+second_number = 0.5
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
